76d5c72a17760ba815de3329f8e701a84d30d6b3
Marcelo Oliveira <finfor.ltda@gmail.com>
2021-01-20 19:12:33 -0300

    release 0.1.9-60

--- DEPI-DepositoIdentificado-web/src/br/com/bradseg/depi/depositoidentificado/funcao/action/LoginAction.java
+++ DEPI-DepositoIdentificado-web/src/br/com/bradseg/depi/depositoidentificado/funcao/action/LoginAction.java
@@ -1,7 +1,6 @@
 package br.com.bradseg.depi.depositoidentificado.funcao.action;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -9,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.SessionAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -16,7 +17,6 @@ import br.com.bradseg.bsad.filtrologin.filters.LoginUtils;
 import br.com.bradseg.bsad.filtrologin.vo.LoginVo;
 import br.com.bradseg.bsad.framework.core.exception.IntegrationException;
 import br.com.bradseg.depi.depositoidentificado.util.BaseUtil;
-import br.com.bradseg.depi.depositoidentificado.util.Funcao;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -31,6 +31,8 @@ public class LoginAction extends ActionSupport implements ServletRequestAware, S
 
 	private static final long serialVersionUID = -4322830132167895863L;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginAction.class);
+
 	private HttpServletRequest request;
 	
 	private static final String MSG_LOGIN_USUARIO = "msg.erro.usuario.logado";
@@ -52,6 +54,7 @@ public class LoginAction extends ActionSupport implements ServletRequestAware, S
 			URL url = new URL(www3);
 			this.sessionData.put("www3", url);
 		} catch (MalformedURLException e) {
+			LOGGER.error("Falha ao montar www3", e);
 		}
 	}
 
@@ -60,55 +63,56 @@ public class LoginAction extends ActionSupport implements ServletRequestAware, S
 	 * @return - ação do sistema
 	 */
 	public String execute() {
+//		Funcao<String, Object> requestAttr = new Funcao<String, Object>() {
+//			
+//			@Override
+//			public Object apply(String source) {
+//				return request.getAttribute(source);
+//			}
+//		};
+//
+//		Funcao<String, Object> requestParam = new Funcao<String, Object>() {
+//			
+//			@Override
+//			public Object apply(String source) {
+//				return request.getParameter(source);
+//			}
+//		};
+//		
+//		Funcao<String, Object> requestSession = new Funcao<String, Object>() {
+//			
+//			@Override
+//			public Object apply(String source) {
+//				return request.getSession().getAttribute(source);
+//			}
+//		};
+//		
+//		
+//		System.out.println("PARAMS: ");
+//		printAttributes(request.getParameterNames(), requestParam);
+//
+//		System.out.println("REQUEST: ");
+//		printAttributes(request.getAttributeNames(), requestAttr);
+//		
+//		System.out.println("SESSION: ");
+//		printAttributes(request.getSession().getAttributeNames(), requestSession);
+		
 		LoginVo loginVO = LoginUtils.getLoginObject(this.request);
-		Funcao<String, Object> requestAttr = new Funcao<String, Object>() {
-			
-			@Override
-			public Object apply(String source) {
-				return request.getAttribute(source);
-			}
-		};
 
-		Funcao<String, Object> requestParam = new Funcao<String, Object>() {
-			
-			@Override
-			public Object apply(String source) {
-				return request.getParameter(source);
-			}
-		};
-		
-		Funcao<String, Object> requestSession = new Funcao<String, Object>() {
-			
-			@Override
-			public Object apply(String source) {
-				return request.getSession().getAttribute(source);
-			}
-		};
-		
-		
-		System.out.println("PARAMS: ");
-		printAttributes(request.getParameterNames(), requestParam);
-
-		System.out.println("REQUEST: ");
-		printAttributes(request.getAttributeNames(), requestAttr);
-		
-		System.out.println("SESSION: ");
-		printAttributes(request.getSession().getAttributeNames(), requestSession);
-		
-        if (BaseUtil.isNZB(loginVO) || BaseUtil.isNZB(loginVO)) {
+		if (BaseUtil.isNZB(loginVO) || BaseUtil.isNZB(loginVO)) {
             throw new IntegrationException(getText(MSG_LOGIN_USUARIO));
         }
 		
 		return Action.SUCCESS;
 	}
 
-	private void printAttributes(Enumeration<String> attributeNames, Funcao<String, Object> funcao) {
-		for (Enumeration<String> e = attributeNames; e.hasMoreElements(); ) {
-			String name = e.nextElement();
-			
-			Object attr = funcao.apply(name); 
-			System.out.println(name + ": " + attr);
-		}
-	}
+//	private void printAttributes(Enumeration<String> attributeNames, Funcao<String, Object> funcao) {
+//		for (Enumeration<String> e = attributeNames; e.hasMoreElements(); ) {
+//			String name = e.nextElement();
+//			
+//			Object attr = funcao.apply(name); 
+//			System.out.println(name + ": " + attr);
+//		}
+//	}
 
 }
